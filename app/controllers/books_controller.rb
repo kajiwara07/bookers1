ea40b3eb5d5f04fb60525dbdde16662870c11f33
EC2 Default User <168887841+kajiwara07@users.noreply.github.com>
2024-06-22 14:22:35 +0000

    アプリケーションを作ろう1 -その6-

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,43 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
+    @books = Book.all
   end
 
   def top
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to 'books'
+    @book = Book.new(book_params)
+   if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+   else
+    @books = Book.all
+    flash.now[:alert] = "Posting failed"
+    render :new
+   end
   end
-
   def index
     @books = Book.all
   end
 
   def show
-
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def change
+    rename_colum :books, :title, :本のタイトル
+    rename_colum :books, :body, :感想
+  end
+  def update
+  @book = Book.find(params[:id])
+  @book.update(book_params)
+   redirect_to book_path(@book.id)
   end
 
   private
